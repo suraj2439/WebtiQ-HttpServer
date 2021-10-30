b79381f544f6e8220cdc3acbc9a35ea62a13076f
Avishkar001 <avirandhale001@gmail.com>
2021-10-31 00:49:12 +0530

    Handling other get headers

--- main.py
+++ main.py
@@ -3,15 +3,35 @@ from _thread import *
 import utility
 from urllib.parse import urlparse
 import os
+from datetime import datetime
+import hashlib
+import time
+
+"""
+Cookie, local time and gmt time, content-type
+
+Accept
+Accept-Charset
+Content-Type
+User-Agent
+Content-Location
+Location       TODO Use in POST request
+Set-Cookie
+Transfer-Encoding
+Connection Keep-Alive
+"""
 
 
-SERVER_PORT = 8001
+
+SERVER_PORT = 7000
 MAX_CONN = 100
 MAX_REQ = 50
 MAX_URI_LENGTH = 500
 MAX_HEADER_LENGTH = 500
+TIME_DIFF = 19800
 SUPPORTED_METHODS = ["GET", "POST", "PUT", "DELETE", "HEAD"]
-DEFAULT_DIR_PATH = ""
+DEFAULT_DIR_PATH = "/home/suraj/Documents/study/TY/CN/Project/"
+EXPIRE_TIME = 86400
 
 req_count = 0
 
@@ -57,18 +77,15 @@ def parse_request(request):
     first_line = header_lines[0].split()
     if(len(first_line) != 3):
         # TODO crosscheck error code
-        response = generate_error_response(400, "Bad Request", "request format is not supported.")
-        return response
+        return {"isError": True, "Status-Code": 400, "Status-Phrase": "Bad Request", "Msg": "request format is not supported."}
     
     [req_method, req_uri, http_version] = first_line
 
     if(isError(len(req_uri), "uri_too_long")):
-        response = generate_error_response(414, "URI Too Long", "Requested uri is too long to handle to server.")
-        return response
+        return {"isError": True, "Status-Code": 414, "Status-Phrase": "URI Too Long", "Msg": "Requested uri is too long to handle to server."}
     
     if(isError(req_method, "method_not_implemented")):
-        response = generate_error_response(501, "Method Not Implemented", "Requested method is not implemented at server side or server could not support requested method.")
-        return response
+        return {"isError": True, "Status-Code": 405, "Status-Phrase": "Method Not Implemented", "Msg": "Requested method is not implemented at server side or server could not support requested method."}
     
     """if(isError(http_version, "version_not_supported")):
         response = generate_error_response(505, "HTTP Version Not Supported", "HTTP Version Not Supported, either requested wrong version format or requested http version is not supported by server.")
@@ -83,54 +100,151 @@ def parse_request(request):
             response = generate_error_response(400, "Bad Request", "Header format is incorrect.")
             return response
         if(isError(len(single_header[1]), "header_too_long")):
-            response = generate_error_response(431, "Request header fields too large", "Requested header field is too large to handle to server.")
-            return response
+            return {"isError": True, "Status-Code": 431, "Status-Phrase": "Request header fields too large", "Msg": "Requested header field is too large to handle to server."}
         # TODO check for supported 
         single_header[0] = single_header[0].strip()
         single_header[1] = single_header[1].strip()
         
         header_dict[single_header[0]] = single_header[1]
     if(isError(header_dict, "host_not_available")):
-        response = generate_error_response(400, "Bad Request", "Header format is incorrect.")
-        return response
+        return {"isError": True, "Status-Code": 400, "Status-Phrase": "Bad Request", "Msg": "Header format is incorrect."}
 
-    return {"headers" : header_dict, "uri" : req_uri, "method" : req_method, "version" : http_version, "body" : body}
+    return {"isError" : False, "headers" : header_dict, "uri" : req_uri, "method" : req_method, "Version" : http_version, "body" : body}
 
     
 def sendResponse():
     pass
 
 def buildResponse(reqDict):
+    responseDict = {}
     headers = reqDict.get("headers")
     uri = reqDict.get("uri")
     method = reqDict.get("method")
     body = reqDict.get("body")
-    accept = headers.get("accept", "*/*")
-    acceptEncoding = headers.get("accept-encoding", "")
+    accept = headers.get("Accept", "*/*")
+    acceptEncoding = headers.get("Accept-Encoding", "")
     contentEncoding = utility.handleEncodingPriority(acceptEncoding)
     if contentEncoding == None:
-        return generate_error_response(406, "Not Acceptable", "Error in content-encoding header field or server could not handle content-encoding header field.")
-
-    
+        return {"isError": True, "Status-Code": 406, "Status-Phrase": "Not Acceptable", "Msg": "Error in content-encoding header field or server could not handle content-encoding header field." }
+        
     path = urlparse(uri).path
     if path == "/":
         path = "/index.html"
     path = DEFAULT_DIR_PATH + path
     
     if not os.path.isfile(path):
-        return generate_error_response(404, "Not Found", "Could not found requested resource.")
+        return {"isError": True, "Status-Code": 404, "Status-Phrase": "Not Found", "Msg": "Could not found requested resource." }
+    else:
+        fd = open(path, 'rb')
+        fileData = fd.read()
+        fd.close()
+   
+
     """
     TODO
     Doubt: How to provide file according to accept header?
     1. Does same file with different extenstion is available at server side. or
     2. Do we need to convert file type like jpg to png.
     """
+    acceptContent = utility.handleAcceptContentPriority(accept)
+    ifModified = headers.get("If-Modified-Since", utility.toRFC_Date(datetime.fromtimestamp(0)))
+    ifUnmodified = headers.get("If-Unmodified-Since", utility.toRFC_Date(datetime.utcnow()))
+    ifMatch = headers.get('If-Match',"*")
+    ifNoneMatch = headers.get('If-None-Match',"")
     
-    acceptContent = utility.handleAcceptContentPriority()
+    ifMatchArr = ifMatch.split(",")
+    ifNoneMatchArr = ifNoneMatch.split(",")
 
+    # generate Etag
+    Etag = '"' + hashlib.md5((str(os.path.getmtime(path)).encode())).hexdigest() + '"'
 
+    if "if-match" in headers.keys():
+        if Etag in ifMatchArr or ifMatchArr[0] == "*":
+            return {"isError": True, "Status-Code": 412, "Status-Phrase": "Precondition Failed", "Msg": "Could not match given etag." }
+    elif "if-unmodified-since" in headers.keys():
+        date = datetime.strptime(headers["if-unmodified-since"] , "%a, %d %b %Y %H:%M:%S GMT")
+        timeFromHeader = time.mktime(date.timetuple())
+        lastModifiedTime = os.path.getmtime(path) - TIME_DIFF
+        if timeFromHeader < lastModifiedTime:
+            return {"isError": True, "Status-Code": 412, "Status-Phrase": "Precondition Failed", "Msg": "Could not meet if-unmodified-since header requirements." }
+    
+    if "if-none-match" in headers.keys():
+        if "*" in ifNoneMatchArr or Etag in ifNoneMatchArr:
+            return {"isError": True, "Status-Code": 304, "Status-Phrase": "Not Modified", "Msg": "Given resource is not modified." }
+    elif "if-modified-since" in headers.keys():
+        date = datetime.strptime(headers["if-modified-since"] , "%a, %d %b %Y %H:%M:%S GMT")
+        timeFromHeader = time.mktime(date.timetuple())
+        lastModifiedTime = os.path.getmtime(path) - TIME_DIFF
+        if timeFromHeader >= lastModifiedTime:
+            return {"isError": True, "Status-Code": 304, "Status-Phrase": "Not Modified", "Msg": "Given resource is not modified." }
+            
+    if "range" in headers.keys() and (headers["range"].split("="))[0] == "bytes":
+        dataAvailable = True
+        # check for conditions on range header
+        if "if-range" in headers.keys():
+            # if-range header is in Etag format
+            if headers["if-range"][0] == '"':
+                if headers["if-range"] != Etag:
+                    dataAvailable = False
+            # if-range header is in last-modified format
+            else:
+                date = datetime.strptime(headers["if-modified-since"] , "%a, %d %b %Y %H:%M:%S GMT")
+                timeFromHeader = time.mktime(date.timetuple())
+                lastModifiedTime = os.path.getmtime(path) - TIME_DIFF
 
+                if timeFromHeader < lastModifiedTime:
+                    dataAvailable = False
+        
+        if dataAvailable:
+            rangesList = headers["range"].split('=')[1].split(',')
+            if len(rangesList) > 1:
+                return {"isError": True, "Status-Code": 412, "Status-Phrase": "Precondition Failed", "Msg": "" }
+            else:
+                r_range = rangesList[0]
+                # range format = -<value>
+                if r_range[0] == "-":
+                    startPos = 0
+                    endPos = r_range[1:]
+                # range format = <value>-
+                elif r_range[-1] == "-":
+                    startPos = r_range[:-1]
+                    endPos = None
+                # range format = <value>-<value>
+                else:
+                    [startPos, endPos] = r_range.split("-")
+                
+                if int(startPos) <= int(endPos):
+                    responseDict["isError"] = False
+                    responseDict["Status-Code"] = "206"
+                    responseDict["Status-Phrase"] = "Partial Content"
+                    responseDict["headers"] = {}
+                    responseDict["headers"]["Content-Range"] = "bytes " + startPos + "-" + endPos + "/" + str(len(fileData))
+                    responseDict["headers"]["Content-Encoding"] = contentEncoding
+                    responseDict["body"] = utility.encodeData(fileData[startPos : endPos+1], contentEncoding)
+                    responseDict["headers"]["Content-Length"] = len(responseDict["body"])
+                    responseDict["headers"]["Content-MD5"] = hashlib.md5(responseDict["body"]).hexdigest()
+                    return responseDict
 
+        #if "cookie" not in headers.keys():
+        #    response["headers"]["Set-Cookie"] = "sessionId={}".format(random.randint(100000, 1000000))
+    responseDict["isError"] = False
+    responseDict["Status-Code"] = "200"
+    responseDict["Status-Phrase"] = "OK"
+    responseDict["headers"] = {}
+    responseDict["headers"]["Last-Modified"] = utility.toRFC_Date(datetime.fromtimestamp(int(os.path.getmtime(path)) - TIME_DIFF))
+    responseDict["headers"]["Content-Encoding"] = contentEncoding
+    #responseDict["Content-Type"] = content_type
+    responseDict["headers"]["E-tag"] = Etag
+    responseDict["headers"]["Accept-Ranges"] = "bytes"
+    responseDict["body"] = utility.encodeData(fileData, contentEncoding)
+    responseDict["headers"]["Content-Length"] = len(responseDict["body"])
+    responseDict["headers"]["Content-MD5"] = hashlib.md5(responseDict["body"]).hexdigest()
+    responseDict["headers"]["Expires"] = utility.toRFC_Date(datetime.fromtimestamp(int(time.time()) + EXPIRE_TIME))
+
+    return responseDict
+
+    # else:
+    #     return {"isError": True, "Status-Code": 412, "Status-Phrase": "Precondition Failed", "Msg": "Could not meet range header requirements." }
 
 def new_thread(client_conn, client_addr):
     req = b''
@@ -141,14 +255,42 @@ def new_thread(client_conn, client_addr):
             break
 
     reqDict = parse_request(req.decode())
-    if(not isinstance(reqDict, dict)):
-        sendResponse()
+    if reqDict["isError"]:
+        content = generate_error_response(reqDict["Status-Code"], reqDict["Status-Phrase"], reqDict["Msg"])
+        responseDict = { "Version": "HTTP/1.1", "Status-Code": str(reqDict["Status-Code"]), "Status-Phrase": reqDict["Status-Phrase"], 
+            "headers": {"Date": utility.toRFC_Date(datetime.utcnow()), "Server": utility.serverInfo(), "Connection": "close" , "Content-Length": str(len(content.encode())) }, 
+            "body": content.encode()}
+        if str(reqDict["Status-Code"]) == "405":
+            responseDict["headers"]["Allow"] = "GET, HEAD, PUT, POST, DELETE"
+
+        client_conn.send(utility.generateResponse(responseDict))
+
     else:
-        buildResponse(reqDict)
+        response = buildResponse(reqDict)
+        resp = { "Version": "HTTP/1.1", "Status-Code": str(response["Status-Code"]), "Status-Phrase": response["Status-Phrase"], 
+            "headers": {"Date": utility.toRFC_Date(datetime.utcnow()), "Server": utility.serverInfo(), "Connection": "close", "Content-Length": ""  }, 
+            "body": ""}
 
-    print(reqDict)
+        if response["isError"]:
+            content = generate_error_response(response["Status-Code"], response["Status-Phrase"], response["Msg"])
+            resp["headers"]["Content-Length"] = str(len(content.encode()))
+            resp["body"] = content.encode()
+        else:
+            resp["body"] = response["body"]
+            resp["headers"]["Content-Length"] = response["headers"]["Content-Length"]
+            resp["headers"].update(response["headers"])
 
+        client_conn.send(utility.generateResponse(resp))
+
+
+
+    """
+    httpVersion
+    statuscode
+    phrase
+    response header dict
     
+    """
 
 
 def generate_error_response(errorCode, errorPhrase, errorMsg):
@@ -165,6 +307,8 @@ def generate_error_response(errorCode, errorPhrase, errorMsg):
     return resp
 
 def main():
+    path = "/home/suraj/Documents/study/TY/CN/Project/http-server/conditional_get_flowchart.png"
+    # print(os.path.getatime("/home/suraj/Documents/study/TY/CN/Project/http-server/rfc_notes.txt"))
     s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_addr = ("localhost", SERVER_PORT)
     s_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -176,6 +320,7 @@ def main():
         if(not isError(req_count, "req_too_long")):
             start_new_thread(new_thread, (client_conn, client_addr))
         else:
+            # temporarily server could not serve the request
             response = generate_error_response(503, "Service Unavailable")
 
     listening_socket.close()
--- utility.py
+++ utility.py
@@ -1,5 +1,14 @@
+import platform
+import gzip
+import zlib
+import brotli
+import lzw3
+
+
 
 def handleEncodingPriority(val):
+    if val == "":
+        return "identity"
     availableEncodings = ["br","compress", "deflate", "gzip", "exi", "pack200-gzip", 
         "x-compress","x-gzip", "zstd"]
     processedEncodings = {}
@@ -26,3 +35,37 @@ def handleEncodingPriority(val):
 
 def handleAcceptContentPriority(val):
     pass
+
+def serverInfo():
+    name = "MY-HTTP-SERVER"
+    version = "1.1"
+    operatingSys = platform.platform()
+    return name + "/" + version + " " + operatingSys
+
+def toRFC_Date(date):
+    weekdayDict = { 0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
+    monthDict = { 1:"Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"} 
+    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekdayDict[date.weekday()], date.day, monthDict[date.month], date.year, date.hour, date.minute, date.second)
+
+def generateResponse(respDict):
+    firstLine = respDict["Version"] + " " + respDict["Status-Code"] + " " + respDict["Status-Phrase"] + "\r\n"
+
+    body = respDict["body"]
+    result = firstLine
+    for key in respDict["headers"]:
+        result += key + ": " + str(respDict["headers"][key]) + "\r\n"
+    result += "\r\n"    
+    result = result.encode() + body
+    return result
+
+def encodeData(data, encodeFormat):
+    if encodeFormat == "gzip" or encodeFormat == "x-gzip":
+        return gzip.compress(data)
+    elif encodeFormat == "compress":
+        return lzw3.compress(data)
+    elif encodeFormat == "deflate":
+        return zlib.compress(data)
+    elif encodeFormat == "br":
+        return brotli.compress(data)
+    return data
+    
